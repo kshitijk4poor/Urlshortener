ab1de896eed05aca3820185b48e2f9253f686a0f
Varun Sharma <varun.pr0coder@gmail.com>
2021-08-05 18:11:35 +0530

    Sab thik kardiya bhai

--- server.js
+++ server.js
@@ -7,38 +7,39 @@ const ShortUrl = require('./models/shortUrl')
 mongoose.connect('mongodb://localhost/urlShortener', {
     useNewUrlParser: true, useUnifiedTopology: true
 })
+
 // serves css as static
 app.use(express.static(path.join(__dirname,"public")));
-app.set('view', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
+
 //render HTML
 app.get('/', async (req, res) => {
     const shortUrls = await ShortUrl.find()
     res.sendFile(__dirname + "/index.html", { shortUrls: shortUrls });
 
 })
+
 //shortUrls
 app.post('/shortUrls', async (req, res) => {
     if(!req.body.shortUrl || !req.body.fullUrl){
         return res.send("No full url/short url provided")
     }
-    const urlExists = await ShortUrl.exists({short: req.body.ShortUrl});
-    if(urlExists){
+    const urlExists = await ShortUrl.find({short: req.body.shortUrl})
+    if(urlExists.length != 0){
         return res.send("shortUrl already exists");
     };
+    console.log(urlExists)
     try{
         await ShortUrl.create({ full: req.body.fullUrl,short:req.body.shortUrl }).then(shortUrl => {
             return res.render('url', {shortUrl: shortUrl})
         });
         
     }catch(e){
-        res.send("Database error")
+        res.send("Database error"+e)
         throw e;
     }
-  
-    res.redirect('/')
 })
 app.get("/:url",async (req,res) => {
     const url = await ShortUrl.findOne({short: req.params.url});
@@ -47,5 +48,6 @@ app.get("/:url",async (req,res) => {
     }
     return res.redirect(url.full);
 })
+
 //runs on port 5000
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => {console.log("om")});
